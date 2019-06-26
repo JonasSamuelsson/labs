d0594b4394897c819e627921360be3dd2994044d
Jonas Samuelsson <jonassamuelsson@outlook.com>
2019-06-26 08:02:51 +0200

    added CryptoKeyGenerator

---
+++ CryptoKeyGenerator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace CryptoKeyGenerator
+{
+   public class Program
+   {
+      public static void Main()
+      {
+         foreach (var keySize in new[] { 16, 32, 64 })
+         {
+            using (var random = new RNGCryptoServiceProvider())
+            {
+               var bytes = new byte[keySize];
+
+               random.GetBytes(bytes);
+
+               Console.WriteLine($"{keySize} bytes ({keySize * 8}-bit): {Convert.ToBase64String(bytes)}");
+            }
+         }
+      }
+   }
+}
